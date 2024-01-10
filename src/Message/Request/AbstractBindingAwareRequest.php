e288b7de024a7d234e7a4d909bcd2f278b217c7e
Poghos Boyajyan <44973047+k3rnel@users.noreply.github.com>
2024-01-10 17:56:01 +0400

    Fixed issue with binding username validation

--- src/Message/Request/AbstractBindingAwareRequest.php
+++ src/Message/Request/AbstractBindingAwareRequest.php
@@ -4,19 +4,6 @@ namespace Omnipay\Arca\Message\Request;
 
 abstract class AbstractBindingAwareRequest extends AbstractRequest
 {
-    /**
-     * Get the raw data array for this message. The format of this varies from gateway to
-     * gateway, but will usually be either an associative array, or a SimpleXMLElement.
-     *
-     * @return array
-     */
-    public function getData() : array
-    {
-        $this->validate('bindingUsername', 'password');
-
-        return parent::getData();
-    }
-
     public function getBindingId() : string
     {
         return $this->getParameter('bindingId');
